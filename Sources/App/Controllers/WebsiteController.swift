34fb159dcf696df643ecbc8fda3284fecddee4dd
Roland Lariotte <roland.sound@live.fr>
2019-09-06 21:19:28 +0200

    Create the base to handle any app privacy policy

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -2,11 +2,25 @@ import Vapor
 import Leaf
 
 struct WebsiteController: RouteCollection {
+  //MARK: - WebsiteController routes
   func boot(router: Router) throws {
-    router.get(use: AirsoftCalculatorPrivacyPolicy)
+    //MARK: AirsoftCalculator
+    router.get("en", "airsoftcalculator", use: AirsoftCalculatorPrivacyPolicy)
   }
-  
+  //MARK: - Handling routes methods
+  //MARK: To show AirsoftCalculator.app Privacy Policy in English
   func AirsoftCalculatorPrivacyPolicy(_ req: Request) throws -> Future<View> {
-    return try req.view().render("AirsoftCalculator")
+    let context = IndexContext(language: "en",
+                               title: "Privacy Policy",
+                               appName: "AirsoftCalculator"
+    )
+    return try req.view().render("enAirsoftCalculator", context)
   }
+}
+
+//MARK: - To insert a title in website title
+struct IndexContext: Encodable {
+  let language: String
+  let title: String
+  let appName: String
 }
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -15,7 +15,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
   
   // Register middleware
   var middlewares = MiddlewareConfig() // Create _empty_ middleware config
-  // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
+  middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
   middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
   services.register(middlewares)
   
@@ -33,4 +33,9 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 //  services.register(migrations)
   
   config.prefer(LeafRenderer.self, for: ViewRenderer.self)
+  services.register { container -> LeafTagConfig in
+    var config = LeafTagConfig.default()
+    config.use(Raw(), as: "raw")
+    return config
+  }
 }
